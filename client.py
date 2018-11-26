817719e0462616a67e24db747663752b4315c5ea
Alex Mullins <jalexmullins91@gmail.com>
2018-11-25 23:11:45 -0600

    Cleaned up common and added to client

--- client.py
+++ client.py
@@ -1,1 +1,43 @@
 # Client Code
+import socket
+from common import *
+from datetime import datetime
+
+
+class NTPeeClient():
+    def __init__(self, dest_ip, dest_port):
+        self.hostport = (dest_ip, dest_port)
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    def get_new_time(self):
+        # will do a round trip request/response
+        # and return the new time to sync to. Return None
+        # on an error
+        cr = ClientRequest(datetime.now().timestamp())
+        data = cr.encode()
+        try:
+            self.client.sendto(data, self.hostport)
+        except:
+            print("error: Could send request to remote server at {}".format(
+                self.hostport))
+            return None
+        try:
+            (new_data, _) = self.client.recvfrom(24)
+        except:
+            print("error: Could not recv data from remote server at {}".format(
+                self.hostport))
+        sr = ServerResponse()
+        sr.decode(new_data)
+        return sr
+
+
+def main():
+    # open a client
+    # poll every 30 seconds
+    # get new time to sync to
+    # apply either instant time jump or gradual time slewing
+    pass
+
+
+if __name__ == "__main__":
+    main()
--- common.py
+++ common.py
@@ -1,5 +1,4 @@
 # Common code
-import pathlib
 import enum
 import abc
 import struct
@@ -12,15 +11,6 @@ def debug_print(msg):
         print(msg)
 
 
-class ConnectionClosedException(Exception):
-    pass
-
-
-class UnknownMsgTypeException(Exception):
-    def __init__(self, code):
-        self.code = code
-
-
 class Message(metaclass=abc.ABCMeta):
     """
     Abstract base class for all MessageTypes.
@@ -28,12 +18,6 @@ class Message(metaclass=abc.ABCMeta):
     to a byte array, and decode a byte array and
     update itself.
     """
-    @abc.abstractmethod
-    def id(self):
-        """
-        Should return the MsgType id for the 
-        subclassed Message.
-        """
 
     @abc.abstractmethod
     def encode(self):
@@ -53,11 +37,6 @@ class Message(metaclass=abc.ABCMeta):
         """
 
 
-class MsgType(enum.IntEnum):
-    ClientRequest = 1
-    ServerResponse = 2
-
-
 class ClientRequest(Message):
     """
     ClientRequest is sent between a NTP client and server
@@ -67,9 +46,6 @@ class ClientRequest(Message):
 
     def __init__(self, t1=None):
         self.t1 = t1
-
-    def id(self):
-        return MsgType.ClientRequest
 
     def encode(self):
         frame = bytearray()
@@ -91,9 +67,6 @@ class ServerResponse(Message):
         self.t2 = t2
         self.t3 = t3
 
-    def id(self):
-        return MsgType.ServerResponse
-
     def encode(self):
         frame = bytearray()
         frame.extend(struct.pack(">d", self.t1))
@@ -106,77 +79,3 @@ class ServerResponse(Message):
         self.t1 = struct.unpack(">d",  data[0:8])[0]
         self.t2 = struct.unpack(">d",  data[8:16])[0]
         self.t3 = struct.unpack(">d",  data[16:24])[0]
-
-
-# dictionary mapping MsgType -> Message sublcasses
-messages = dict()
-messages[MsgType.ClientRequest] = ClientRequest
-messages[MsgType.ServerResponse] = ServerResponse
-
-
-def recvmsg(socket):
-    """
-    recvmsg will read an effteepee protocol message
-    from the socket. it will return a tuple 
-    (msgid, msg) where msgid is the id for the MsgType
-    and msg is a structure matching the MsgType. 
-    """
-    # recv the id
-    # recv the 2-byte msg len.
-    # read msg len bytes from the socket.
-    # pass data to parse method to return structure.
-    msgid = recvid(socket)
-    if not msgid in messages:
-        raise UnknownMsgTypeException(msgid)
-    msglen = int.from_bytes(recvall(socket, 2), byteorder="big")
-    data = recvall(socket, msglen)
-    msgtype = messages[msgid]
-    msg = msgtype()
-    msg.decode(data)
-    return (msgid, msg)
-
-
-def wrap_in_id_length(msgid, data):
-    msglen = len(data)
-    frame = bytearray()
-    frame.extend(int(msgid).to_bytes(1, byteorder="big"))
-    frame.extend((msglen).to_bytes(2, byteorder="big"))
-    frame.extend(data)
-    return bytes(frame)
-
-
-def sendmsg(socket, msg):
-    """
-    sendmsg will send an effteepee protocol message
-    on the socket. Can raise UnknownMsgTypeException.
-    """
-    if not msg.id() in messages:
-        raise UnknownMsgTypeException(msg.id())
-    data = msg.encode()
-    data = wrap_in_id_length(msg.id(), data)
-    socket.sendall(data)
-
-
-def recvid(socket):
-    """
-    recvid will receive a message's id 
-    and return a MsgType.
-    """
-    rid = recvall(socket, 1)
-    msgid = MsgType(int.from_bytes(rid, byteorder="big"))
-    return msgid
-
-
-def recvall(socket, n):
-    """
-    recvall will receive all n bytes of information
-    and return it as a bytes sequence. Raises a 
-    ConnectionClosedException if the socket closes.
-    """
-    frame = bytearray()
-    while len(frame) < n:
-        packet = socket.recv(n - len(frame))
-        if not packet:
-            raise ConnectionClosedException()
-        frame.extend(packet)
-    return bytes(frame)
